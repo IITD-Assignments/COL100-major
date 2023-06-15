98e036d4d6657cc4aa812d93f5d74c95024736d1
Harsha Vardhan <x.projekt@outlook.com>
2023-06-15 17:46:22 +0530

    feat: create g361.py

---
+++ g361.py
@@ -0,0 +1,11 @@
+def max_value(V, S):
+    l = []
+    for i in range(len(V)):
+        # V[i] += V[i+S[i]]  # fix C to S
+        res = 0  # I added this
+        for j in range(len(S)):
+            res += V[i] + V[i+S[i]]
+        
+        l.append(res)
+
+    return max(l)
