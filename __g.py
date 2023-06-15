ffa6db6901a268f45b41c670e17a6163e442c38d
Harsha Vardhan <x.projekt@outlook.com>
2023-06-15 18:07:49 +0530

    fix: update code to check max_value_indices()

--- __g.py
+++ __g.py
@@ -1,2 +1,5 @@
 def max_value(V, S):
     pass
+
+def max_value_indices(V, S):
+    pass
--- run.py
+++ run.py
@@ -10,7 +10,11 @@ if __name__ == "__main__":
 
     V = [4, 3, 1, 2, 1]
     S = [2, 1, 1, 1, 1]
-    ret = mod.max_value(V, S)
-    if ret is not None:
-        print(ret)
-    
+
+    ret_1 = mod.max_value(V, S)
+    if ret_1 is not None:
+        print(ret_1)
+
+    ret_2 = mod.max_value_indices(V, S)
+    if ret_2 is not None:
+        print(ret_2)
