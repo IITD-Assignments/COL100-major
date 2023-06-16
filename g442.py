a851ed3fa2c3d431434938193666a69b7554bd89
Harsha Vardhan <x.projekt@outlook.com>
2023-06-16 08:55:39 +0530

    feat: create g442.py

---
+++ g442.py
@@ -0,0 +1,11 @@
+def max_value(V, S):
+    if len(V) == 1:
+        return V[0]
+    elif len(V) == 2:
+        return max(V[0], V[1])
+    elif len(V) == 3:
+        pass
+        # didn't write rest of the code
+
+def max_value_indices(V, S):
+    pass
